f300639c04a3c0219e80084e67c95b19db20310c
John Cler <choquejohn6@gmail.com>
2024-05-17 18:32:05 -0400

    Creando proyecto Flutter

---
+++ lib/authServices.dart
@@ -0,0 +1,14 @@
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthServices {
+  iniciarSesion() async {
+    final GooglecrearCuenta? gUser = await GoogleSignIn().signIn();
+
+    final GoogleAutenticaction gAuth = await gUser!;
+
+    final credencial = GoogleAuhtProvider.credencial(
+      accesToken: gAuth.accesToken,
+      idToken: gAuth.idToken,
+    );
+  }
+}
